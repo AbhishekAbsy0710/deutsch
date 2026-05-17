a2bfaaa469650adfaff268d43dba24c7cb8264ce
AbhishekKyaani <abhishek@kyaani.com>
2026-05-17 14:55:14 +0200

    vocab: batch 51 - directions, celebrations, banking, doctor, education, housing, neuroscience, urban planning, volcanology, numismatics, neurophenomenology, biomimetics (+78 unique, TOTAL: 7082)

--- src/data/lessons.ts
+++ src/data/lessons.ts
@@ -49,6 +49,7 @@ import { lessonsA1Mega30 } from "./lessons-a1-mega30";
 import { lessonsA1Mega31 } from "./lessons-a1-mega31";
 import { lessonsA1Mega32 } from "./lessons-a1-mega32";
 import { lessonsA1Mega33 } from "./lessons-a1-mega33";
+import { lessonsA1Mega34 } from "./lessons-a1-mega34";
 import { lessonsA1New5, lessonsA2New9 } from "./lessons-final-gaps";
 import { lessonsA1Final, lessonsA2Final } from "./lessons-final-gaps2";
 import { lessonsA2ExamSkills } from "./lessons-a2-exam";
@@ -98,6 +99,7 @@ import { lessonsA2Mega28 } from "./lessons-a2-mega28";
 import { lessonsA2Mega29 } from "./lessons-a2-mega29";
 import { lessonsA2Mega30 } from "./lessons-a2-mega30";
 import { lessonsA2Mega31 } from "./lessons-a2-mega31";
+import { lessonsA2Mega32 } from "./lessons-a2-mega32";
 import { lessonsB1 } from "./lessons-b1";
 import { lessonsB1Grammar1 } from "./lessons-b1-grammar1";
 import { lessonsB1Grammar2 } from "./lessons-b1-grammar2";
@@ -148,6 +150,7 @@ import { lessonsB1Mega31 } from "./lessons-b1-mega31";
 import { lessonsB1Mega32 } from "./lessons-b1-mega32";
 import { lessonsB1Mega33 } from "./lessons-b1-mega33";
 import { lessonsB1Mega34 } from "./lessons-b1-mega34";
+import { lessonsB1Mega35 } from "./lessons-b1-mega35";
 import { lessonsB2 } from "./lessons-b2";
 import { lessonsB2Grammar1 } from "./lessons-b2-grammar1";
 import { lessonsB2Grammar2 } from "./lessons-b2-grammar2";
@@ -195,6 +198,7 @@ import { lessonsB2Mega29 } from "./lessons-b2-mega29";
 import { lessonsB2Mega30 } from "./lessons-b2-mega30";
 import { lessonsB2Mega31 } from "./lessons-b2-mega31";
 import { lessonsB2Mega32 } from "./lessons-b2-mega32";
+import { lessonsB2Mega33 } from "./lessons-b2-mega33";
 import { lessonsC1Partikeln } from "./lessons-c1-partikeln";
 import { lessonsC1Grammar } from "./lessons-c1-grammar";
 import { lessonsC1Grammar2 } from "./lessons-c1-grammar2";
@@ -260,6 +264,7 @@ import { lessonsC1Mega29 } from "./lessons-c1-mega29";
 import { lessonsC1Mega30 } from "./lessons-c1-mega30";
 import { lessonsC1Mega31 } from "./lessons-c1-mega31";
 import { lessonsC1Mega32 } from "./lessons-c1-mega32";
+import { lessonsC1Mega33 } from "./lessons-c1-mega33";
 import { lessonsC2Mega1 } from "./lessons-c2-mega1";
 import { lessonsC2Mega2 } from "./lessons-c2-mega2";
 import { lessonsC2Mega3 } from "./lessons-c2-mega3";
@@ -290,6 +295,7 @@ import { lessonsC2Mega27 } from "./lessons-c2-mega27";
 import { lessonsC2Mega28 } from "./lessons-c2-mega28";
 import { lessonsC2Mega29 } from "./lessons-c2-mega29";
 import { lessonsC2Mega30 } from "./lessons-c2-mega30";
+import { lessonsC2Mega31 } from "./lessons-c2-mega31";
 
 export type LessonBlock = 
   | { type: "vocabulary", word: string, translation: string, example: string, audioUrl?: string, phonetic: string }
@@ -365,6 +371,7 @@ export const lessonData: Record<string, Lesson> = {
   ...lessonsA1Mega31,
   ...lessonsA1Mega32,
   ...lessonsA1Mega33,
+  ...lessonsA1Mega34,
   ...lessonsA2,
   ...lessonsA2New,
   ...lessonsA2New2,
@@ -414,6 +421,7 @@ export const lessonData: Record<string, Lesson> = {
   ...lessonsA2Mega29,
   ...lessonsA2Mega30,
   ...lessonsA2Mega31,
+  ...lessonsA2Mega32,
   ...lessonsB1,
   ...lessonsB1Grammar1,
   ...lessonsB1Grammar2,
@@ -464,6 +472,7 @@ export const lessonData: Record<string, Lesson> = {
   ...lessonsB1Mega32,
   ...lessonsB1Mega33,
   ...lessonsB1Mega34,
+  ...lessonsB1Mega35,
   ...lessonsB2,
   ...lessonsB2Grammar1,
   ...lessonsB2Grammar2,
@@ -512,6 +521,7 @@ export const lessonData: Record<string, Lesson> = {
   ...lessonsB2Mega30,
   ...lessonsB2Mega31,
   ...lessonsB2Mega32,
+  ...lessonsB2Mega33,
   ...lessonsC1Partikeln,
   ...lessonsC1Grammar,
   ...lessonsC1Grammar2,
@@ -577,6 +587,7 @@ export const lessonData: Record<string, Lesson> = {
   ...lessonsC1Mega30,
   ...lessonsC1Mega31,
   ...lessonsC1Mega32,
+  ...lessonsC1Mega33,
   ...lessonsC2Mega1,
   ...lessonsC2Mega2,
   ...lessonsC2Mega3,
@@ -607,4 +618,5 @@ export const lessonData: Record<string, Lesson> = {
   ...lessonsC2Mega28,
   ...lessonsC2Mega29,
   ...lessonsC2Mega30,
+  ...lessonsC2Mega31,
 };
